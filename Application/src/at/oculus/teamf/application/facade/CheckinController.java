61fefdc7eb67a75ce42038ade77237459f268235
Norskan <simon.angerer@hotmail.com>
2015-04-02 16:32:13 +0200

    Database Connection:
    Basic implemntation

---
+++ Application/src/at/oculus/teamf/application/facade/CheckinController.java
@@ -0,0 +1,28 @@
+package at.oculus.teamf.application.facade;
+
+import at.oculus.teamf.domain.entity.PatientEntity;
+import at.oculus.teamf.persistence.Facade;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+/**
+ * Created by Norskan on 02.04.2015.
+ */
+public class CheckinController {
+
+    public PatientEntity getPatient(int svn){
+        Collection <Class> collection = new LinkedList<Class>();
+        collection.add(PatientEntity.class);
+        Facade facade = Facade.getInstance(collection);
+        PatientEntity patient = (PatientEntity) facade.getEnity(PatientEntity.class, 1);
+
+        Collection <PatientEntity> patients;
+
+        // = get all, search patient (svn), patient = found patient; return
+
+        return patient;
+    }
+
+
+}
