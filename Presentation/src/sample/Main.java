addd84fc3b85b23c01e51fdd3f64bded57d8894d
karolineDe <karoline.deuring@students.fhv.at>
2015-04-08 15:17:21 +0200

    new window for calendar

--- Presentation/src/sample/Main.java
+++ Presentation/src/sample/Main.java
@@ -1,12 +1,16 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.IOException;
 
 public class Main extends Application {
+
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -18,6 +22,7 @@ public class Main extends Application {
         primaryStage.setMaximized(true);
         primaryStage.show();
     }
+
 
 
     public static void main(String[] args) {
--- Presentation/src/sample/SampleController.java
+++ Presentation/src/sample/SampleController.java
@@ -13,15 +13,23 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class SampleController implements Initializable{
 
     @FXML private ListView wList1, wList2, wList3, wListO;
+    @FXML Stage _agendaStage;
 
     @Override
     public void initialize(URL location, ResourceBundle resources)
@@ -31,8 +39,22 @@ public class SampleController implements Initializable{
         wList1.setItems(wList);
     }
 
+    /*
+        Close Main Window with MenuItem in Menubar
+     */
+    @FXML
     public void onClose(ActionEvent actionEvent)
     {
         System.exit(0);
+    }
+
+
+    public void openCal() throws IOException
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("agenda.fxml"));
+        Stage _agendaStage = new Stage();
+        Scene agendaScene = new Scene(root);
+        _agendaStage.setScene(agendaScene);
+        _agendaStage.show();
     }
 }
