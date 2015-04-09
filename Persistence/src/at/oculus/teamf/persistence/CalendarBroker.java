17a9de913101c832338eb935e2fcc35258c70973
Norskan <simon.angerer@hotmail.com>
2015-04-09 17:40:15 +0200

    Git gewurstel

--- Persistence/src/at/oculus/teamf/persistence/CalendarBroker.java
+++ Persistence/src/at/oculus/teamf/persistence/CalendarBroker.java
@@ -9,8 +9,6 @@
 
 package at.oculus.teamf.persistence;
 
-import at.oculus.teamf.databaseconnection.session.BadSessionException;
-import at.oculus.teamf.databaseconnection.session.ClassNotMappedException;
 import at.oculus.teamf.databaseconnection.session.ISession;
 import at.oculus.teamf.domain.entity.Calendar;
 import at.oculus.teamf.domain.entity.CalendarEvent;
@@ -18,10 +16,7 @@ import at.oculus.teamf.persistence.entities.CalendarEntity;
 import at.oculus.teamf.persistence.entities.CalendarEventEntity;
 import at.oculus.teamf.persistence.exceptions.FacadeException;
 import at.oculus.teamf.persistence.exceptions.InvalideReloadParameterExeption;
-import at.oculus.teamf.persistence.exceptions.NoBrokerMappedException;
-import at.oculus.teamf.persistence.exceptions.NotAbleToLoadClassException;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -34,7 +29,7 @@ class CalendarBroker extends EntityBroker<Calendar, CalendarEntity> implements I
 	}
 
 	@Override
-	protected Calendar persitentToDomain(CalendarEntity entity) {
+	protected Calendar persitentToDomain(CalendarEntity entity) throws FacadeException {
 		Calendar calendar = new Calendar();
 		calendar.setCalendarID(entity.getId());
 		return calendar;
@@ -48,62 +43,24 @@ class CalendarBroker extends EntityBroker<Calendar, CalendarEntity> implements I
 
 	@Override
 	public void reload(ISession session, Object obj, Class clazz) throws FacadeException {
-		if (clazz == CalendarEvent.class) {
-			((Calendar)obj).setEvents(reloadCalendarEvents(session, obj));
+		if (clazz == at.oculus.teamf.domain.entity.CalendarEvent.class) {
+			((Calendar) obj).setEvents(reloadCalendarEvents(session, obj));
 		} else {
 			throw new InvalideReloadParameterExeption();
 		}
 	}
 
-	//Todo: extract into generic reload component
+	private class CalendarEventsLoader extends CollectionLoader<CalendarEventEntity> {
+
+		@Override
+		public Collection<CalendarEventEntity> load(Object databaseEntity) {
+			return ((CalendarEntity) databaseEntity).getCalendarEvents();
+		}
+	}
+
 	private Collection<CalendarEvent> reloadCalendarEvents(ISession session, Object obj) throws FacadeException {
-		Facade facade = Facade.getInstance();
+		ReloadComponent<CalendarEvent, CalendarEventEntity> reloadComponent = new ReloadComponent<CalendarEvent, CalendarEventEntity>(CalendarEntity.class, CalendarEvent.class);
 
-		//get broker
-		EntityBroker broker = null;
-		try {
-			broker = facade.getBroker(obj.getClass());
-		} catch (NoBrokerMappedException e) {
-			//Todo: add Loging
-			e.printStackTrace();
-
-			throw new NotAbleToLoadClassException();
-		}
-
-		//load calendar
-
-		CalendarEntity calendarEntity = null;
-		try {
-			calendarEntity = (CalendarEntity) session.getByID(CalendarEntity.class, ((Calendar)obj).getCalendarID());
-		} catch (BadSessionException e) {
-			e.printStackTrace();
-		} catch (ClassNotMappedException e) {
-			e.printStackTrace();
-		}
-		if (calendarEntity == null) {
-			throw new NotAbleToLoadClassException();
-		}
-
-		//load entity collection
-		Collection<CalendarEventEntity> calendarEntities =
-				(Collection<CalendarEventEntity>) (Collection<?>) calendarEntity.getCalendarEvents();
-
-
-		//convert to domain object
-		try {
-			broker = facade.getBroker(CalendarEvent.class);
-		} catch (NoBrokerMappedException e) {
-			//Todo: add Loging
-			e.printStackTrace();
-
-			throw new NotAbleToLoadClassException();
-		}
-
-		Collection<CalendarEvent> calendarEvents = new ArrayList<CalendarEvent>();
-		for(CalendarEventEntity cee : calendarEntities) {
-			calendarEvents.add((CalendarEvent)broker.persitentToDomain(cee));
-		}
-
-		return calendarEvents;
+		return reloadComponent.reloadCollection(session, ((Calendar) obj).getCalendarID(), new CalendarEventsLoader());
 	}
 }
---
+++ Persistence/src/at/oculus/teamf/persistence/ReloadComponent.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2015 Team F
+ *
+ * This file is part of Oculus.
+ * Oculus is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * Oculus is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with Oculus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package at.oculus.teamf.persistence;
+
+import at.oculus.teamf.databaseconnection.session.BadSessionException;
+import at.oculus.teamf.databaseconnection.session.ClassNotMappedException;
+import at.oculus.teamf.databaseconnection.session.ISession;
+import at.oculus.teamf.persistence.exceptions.FacadeException;
+import at.oculus.teamf.persistence.exceptions.NoBrokerMappedException;
+import at.oculus.teamf.persistence.exceptions.NotAbleToLoadClassException;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Reload Component for domain class brokers
+ *
+ * @param <D>
+ * 		Domain class of which the collection needs to be reloaded
+ * @param <R>
+ * 		The reload database entity
+ */
+public class ReloadComponent<D, R> {
+
+	private Class<D> _entityClazz;
+	private Class<R> _clazzToLoad;
+
+	public ReloadComponent(Class entityClazz, Class clazzToLoad) {
+		_entityClazz = entityClazz;
+		_clazzToLoad = clazzToLoad;
+	}
+	/**
+	 * @param session
+	 * 		session to use
+	 * @param id
+	 * 		of the object to reload
+	 * @param loader
+	 * 		that will load the collection
+	 *
+	 * @return collection of entityClazz
+	 *
+	 * @throws FacadeException
+	 * 		gets thrown if an error occures
+	 */
+	public Collection<D> reloadCollection(ISession session, int id, CollectionLoader loader) throws FacadeException {
+		Facade facade = Facade.getInstance();
+
+		//load database CalendarEventEntity that has the collection that needs to be reloaded
+		Object databaseEntity = null;
+		try {
+			databaseEntity = session.getByID(_entityClazz, id);
+		} catch (BadSessionException e) {
+			e.printStackTrace();
+		} catch (ClassNotMappedException e) {
+			e.printStackTrace();
+		}
+		if (databaseEntity == null) {
+			throw new NotAbleToLoadClassException();
+		}
+
+		//load CalendarEventEntity collection from database CalendarEventEntity
+		Collection<R> entities = loader.load(databaseEntity);
+
+
+		//get domain object broker
+		EntityBroker toLoadClassDomainBroker = null;
+		try {
+			toLoadClassDomainBroker = facade.getBroker(_clazzToLoad);
+		} catch (NoBrokerMappedException e) {
+			//Todo: add Loging
+			e.printStackTrace();
+
+			throw new NotAbleToLoadClassException();
+		}
+
+		//convert database entity collection to domain entity collection
+		Collection<D> objects = new ArrayList<D>();
+		for (R r : entities) {
+			objects.add((D) toLoadClassDomainBroker.persitentToDomain(r));
+		}
+
+		return objects;
+	}
+}
