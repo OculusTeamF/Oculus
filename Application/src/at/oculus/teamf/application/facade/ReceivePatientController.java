f7e50de197690d3ea8a78eda5c34c3c026ea4946
theredsun91 <simon.hartmann@students.fhv.at>
2015-04-13 14:39:30 +0200

    commenting of all the files, classes and methods, implementing of the ILogger in the usecases

--- Application/src/at/oculus/teamf/application/facade/ReceivePatientController.java
+++ Application/src/at/oculus/teamf/application/facade/ReceivePatientController.java
@@ -7,11 +7,24 @@
  * You should have received a copy of the GNU General Public License along with Oculus.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**<h1>$ReceivePatientController.java</h1>
+ * @author $jpo2433
+ * @author $sha9939
+ * @since $13.04.15
+ *
+ * Description:
+ * This file contains all the methods which are necessary for the usecase ReceivePatientController. It also contains the class
+ * ReceivePatientController.
+ **/
+
 package at.oculus.teamf.application.facade;
 
 /**
- * Created by jpo2433 on 13.04.15.
- */
+ * <h2>$ReceivePatientController</h2>
+ *
+ * <b>Description:</b>
+ * This class contains all the necessary methods for the usecase ReceivePatient.
+ **/
 public class ReceivePatientController {
 
     /*createUntersuchungsprotokoll holt instanz und gibt sie an presentation layer .
