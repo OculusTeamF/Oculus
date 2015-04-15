9539cc4631e1ed2c06ce1937dff200056bbdb615
karolineDe <karoline.deuring@students.fhv.at>
2015-04-15 12:41:08 +0200

    openPatientRecord() under construction

--- Domain/src/at/oculus/teamf/domain/entity/Patient.java
+++ Domain/src/at/oculus/teamf/domain/entity/Patient.java
@@ -11,9 +11,15 @@ package at.oculus.teamf.domain.entity;
 
 import at.oculus.teamf.persistence.Facade;
 import at.oculus.teamf.persistence.exceptions.FacadeException;
+import javafx.beans.InvalidationListener;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 
 import java.sql.Date;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 /**
  * Todo: add docs, implement equals, getter into interface wrappen
@@ -203,4 +209,5 @@ public class Patient implements IPatient, IDomain {
 	public String toString(){
 		return getFirstName() + " " + getLastName();
 	}
+
 }
