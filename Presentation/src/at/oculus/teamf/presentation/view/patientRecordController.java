c01a50ce85ed2222f19c5753e3046390f4a07778
karolineDe <karoline.deuring@students.fhv.at>
2015-04-24 19:15:24 +0200

    examinationprotocoll

--- Presentation/src/at/oculus/teamf/presentation/view/patientRecordController.java
+++ Presentation/src/at/oculus/teamf/presentation/view/patientRecordController.java
@@ -313,7 +313,7 @@ public class patientRecordController implements Initializable {
 
     public void openExamination(ActionEvent actionEvent) {
         try {
-            Main.controller.getTabPane().getTabs().addAll((Tab) FXMLLoader.load(this.getClass().getResource("fxml/examinationTab.fxml")));
+            Main.controller.getTabPane().getTabs().addAll((Tab) FXMLLoader.load(this.getClass().getResource("fxml/ExaminationTab.fxml")));
             Main.controller.getTabPane().getSelectionModel().select(Main.controller.getTabPane().getTabs().size() - 1);
         } catch (IOException e) {
             e.printStackTrace();
