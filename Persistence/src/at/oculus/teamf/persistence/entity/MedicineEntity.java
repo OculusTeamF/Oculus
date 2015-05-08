7eb5e1909d5207c34677e1b1bc98c31a1b11a6d5
Daniel Griesser <dgr7348@students.fhv.at>
2015-05-08 13:40:49 +0200

    Interfaces

---
+++ Persistence/src/at/oculus/teamf/persistence/entity/MedicineEntity.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2015 Team F
+ *
+ * This file is part of Oculus.
+ * Oculus is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * Oculus is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with Oculus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package at.oculus.teamf.persistence.entity;
+
+/**
+ * MedicineEntity.java
+ * Created by oculus on 06.05.15.
+ */
+public class MedicineEntity {
+    private int _id;
+    private Integer _diagnosisId;
+    private DiagnosisEntity _diagnosis;
+}
