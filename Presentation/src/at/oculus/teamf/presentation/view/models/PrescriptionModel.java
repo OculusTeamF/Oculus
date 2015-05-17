aef08d6b05913ba4603c63490408fc2d9badae53
Fabian Salzgeber <fsa7975@students.fhv.at>
2015-05-17 21:41:12 +0200

    prescription save

--- Presentation/src/at/oculus/teamf/presentation/view/models/PrescriptionModel.java
+++ Presentation/src/at/oculus/teamf/presentation/view/models/PrescriptionModel.java
@@ -13,22 +13,18 @@ import at.oculus.teamf.application.facade.PrescriptionController;
 import at.oculus.teamf.application.facade.VisualAidController;
 import at.oculus.teamf.application.facade.dependenceResolverTB2.exceptions.NotInitatedExceptions;
 import at.oculus.teamf.application.facade.exceptions.NoPatientException;
-import at.oculus.teamf.domain.entity.CantGetPresciptionEntriesException;
-import at.oculus.teamf.domain.entity.VisualAid;
+import at.oculus.teamf.domain.entity.exception.CantGetPresciptionEntriesException;
 import at.oculus.teamf.domain.entity.exception.CouldNotAddPrescriptionEntryException;
-import at.oculus.teamf.domain.entity.exception.CouldNotGetPrescriptionException;
-import at.oculus.teamf.domain.entity.exception.CouldNotGetVisualAidException;
 import at.oculus.teamf.domain.entity.interfaces.*;
 import at.oculus.teamf.persistence.exception.BadConnectionException;
 import at.oculus.teamf.persistence.exception.DatabaseOperationException;
 import at.oculus.teamf.persistence.exception.NoBrokerMappedException;
 import at.oculus.teamf.presentation.view.DialogBoxController;
 import at.oculus.teamf.technical.exceptions.NoPrescriptionToPrintException;
-import javafx.collections.FXCollections;
 import org.apache.pdfbox.exceptions.COSVisitorException;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.Collection;
 
 /**
  * Created by Karo on 11.05.2015.
